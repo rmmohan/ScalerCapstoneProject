979190d0f2ca8efb99944420c7f71ec5d1df331c
Rammohan K Vadlamani <rmmohan@gmail.com>
2025-12-15 17:33:03 +0530

    Update API Gateway with Keycloak, Spring Security, and Swagger UI
    - Add Keycloak service to docker-compose.yml
    - Update pom.xml to include Spring Security and Swagger UI
    - Configure CORS to allow access to Swagger UI
    - Update gateway routes to expose microservices Swagger endpoints

--- api-gateway/src/main/java/com/rv/microservices/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/rv/microservices/gateway/routes/Routes.java
@@ -1,37 +1,65 @@
 package com.rv.microservices.gateway.routes;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
-
-import static org.springframework.web.servlet.function.RequestPredicates.GET;
 
 @Configuration
 public class Routes {
-
+    
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
-        return GatewayRouterFunctions.route("product-service")
-                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
-                .build();
+        return route().GET("/api/product", http())
+            .before(uri("http://localhost:8080"))
+            .build();
     }
-
+    
     @Bean
     public RouterFunction<ServerResponse> orderServiceRoute() {
-        return GatewayRouterFunctions.route("order-service")
-                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+        return route().GET("/api/order", http())
+            .before(uri("http://localhost:8081"))
+            .build();
+    }
+    
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return route().GET("/api/inventory", http())
+            .before(uri("http://localhost:8082"))
+            .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(path("/aggregate/product-service/v3/api-docs"), http())
+                .filter(setPath("/api-docs"))
+                .before(uri("http://localhost:8080"))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> inventoryServiceRoute() {
-        return GatewayRouterFunctions.route("inventory-service")
-                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(path("/aggregate/order-service/v3/api-docs"), http())
+                .filter(setPath("/api-docs"))
+                .before(uri("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(path("/aggregate/inventory-service/v3/api-docs"), http())
+                .filter(setPath("/api-docs"))
+                .before(uri("http://localhost:8082"))
                 .build();
     }
 }
