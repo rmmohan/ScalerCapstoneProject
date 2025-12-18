3a49f7f415fa2ae3a41fde7127696fd3784b5a8e
Rammohan K Vadlamani <rmmohan@gmail.com>
2025-12-18 23:33:30 +0530

    Update Order Service to include circuit breaker
    - update pom.xml to add dependency for circuit breaker and Actuators
    - update InventoryClient to include circuit breaker
    - update application.properties to include circuit breaker and actuator configurations

--- order-service/src/main/java/com/rv/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/rv/microservices/order/client/InventoryClient.java
@@ -1,12 +1,29 @@
 package com.rv.microservices.order.client;
 
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
+@Slf4j
+@FeignClient(name = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skucode {}, failure reason: {}", code, throwable.getMessage());
+        return false;
+    }
 }
