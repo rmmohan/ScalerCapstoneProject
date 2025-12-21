6fbe1d079548c3b73c70cf882ed923b438800074
Rammohan K Vadlamani <rmmohan@gmail.com>
2025-12-21 11:45:32 +0530

    Update API Gateway
    - update CORS configuration for API Gateway

--- api-gateway/src/main/java/com/rv/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/rv/microservices/gateway/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
